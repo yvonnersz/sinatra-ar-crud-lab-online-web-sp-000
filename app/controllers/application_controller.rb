af990396e342437f8fa2d307e1abd475584adf6d
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-29 04:46:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+<<<<<<< HEAD
   get '/' do
     redirect to '/articles'
   end
@@ -49,3 +49,49 @@ class ApplicationController < Sinatra::Base
 
 
 end
+=======
+
+    get '/' do
+      redirect to '/articles'
+    end
+
+    get '/articles' do
+      @articles = Article.all
+      erb :index
+    end
+
+    get '/articles/new' do
+      @article = Article.new
+      erb :new
+    end
+
+    post '/articles' do
+      @article = Article.create(title: params[:title], content: params[:content])
+      redirect to "/articles/#{@article.id}"
+    end
+
+    get '/articles/:id' do
+      @article = Article.find(params[:id])
+      erb :show
+    end
+
+    get '/articles/:id/edit' do
+      @article = Article.find(params[:id])
+      erb :edit
+    end
+
+
+    patch "/articles/:id" do
+      @article = Article.find(params[:id])
+      # @article.update(title: params[:title], content: params[:content])
+      @article.update(params[:article])
+      redirect to "/articles/[:id]"
+    end
+
+
+    delete '/articles/:id' do
+      Article.delete(params[:id])
+      redirect to "/articles"
+    end
+  end
+>>>>>>> 2e8ac4cc0906e3806e1591cb4fb2d38bf6a1751b
